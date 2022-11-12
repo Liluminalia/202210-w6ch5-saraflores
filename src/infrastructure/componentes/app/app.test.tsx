f139b5e7a78a9e3f4e8c609e7317ea4bdcbc9c3d
Liluminalia <peloazul4@gmail.com>
2022-11-12 14:38:42 +0100

    updating app test to cover store

--- src/infrastructure/componentes/app/app.test.tsx
+++ src/infrastructure/componentes/app/app.test.tsx
@@ -1,9 +1,15 @@
 import React from 'react';
 import { render, screen } from '@testing-library/react';
 import { App } from './app';
+import { Provider } from 'react-redux';
+import { appStore } from '../../store/store';
 
-test('renders learn react link', () => {
-    render(<App />);
+test('renders link', () => {
+    render(
+        <Provider store={appStore}>
+            <App />
+        </Provider>
+    );
     const linkElement = screen.getByText(/probando/i);
     expect(linkElement).toBeInTheDocument();
 });
